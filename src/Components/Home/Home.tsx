61103ea4c129982d87b5c722cb9330e9e592b4d6
Rawan Ahmed <RawanSadek267@gmail.com>
2025-05-26 00:41:57 +0300

    updated

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,9 +1,16 @@
-
+import home2 from '../../assets/home2.jpeg'
 
 export default function Home() {
   return (
-    <div>
-      home
+    <div className="homeContainer d-flex justify-content-center text-center p-5">
+      <div className='container'>
+        <div className="textContainer">
+          <h2>Welcome to UMS</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum alias asperiores corrupti accusamus sequi officiis voluptatem rerum! Nisi quia ullam ducimus dignissimos totam accusamus dicta, eveniet magnam hic tempore. Aut, sequi fugit aspernatur similique possimus enim, facilis qui perspiciatis odit voluptate suscipit libero ullam voluptatem numquam id commodi cum mollitia!</p>
+        </div>
+
+        <img src={home2} alt="" className='w-75' />
+      </div>
     </div>
   )
 }
